30a664a7195a1d4c8446fb69522f62d34efff1a5
Constantine.P <pkv989@gmail.com>
2019-12-17 02:03:38 +0600

    Удалил лишние папки с файлами: classes, functions, data

--- functions/doHtmlBlock.js
+++
@@ -1,6 +0,0 @@
-/*
-/!*export default*!/ function doHtmlBlock(obj) {
-    return `<${obj.tag} class="${obj.class}">${obj.text}</${obj.tag}>`
-}
-
-module.exports = doHtmlBlock;*/
--- functions/isEquivalent.js
+++
@@ -1,6 +0,0 @@
-/*
-function isEquivalent(unit_1, unit_2) {
-    return JSON.stringify(unit_1) === JSON.stringify(unit_2);
-}
-
-module.exports = isEquivalent;*/
